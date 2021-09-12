64e77623d7bd0510c86a1c49bdfc54ded7d45b0a
Pravin Jadhav <83411817+pravinpadmakarjadhav@users.noreply.github.com>
2021-09-12 09:44:53 +0530

    UPDATE

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv =document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+
+  return serverURL + "?" +"text=" + text
+}
+
+function errorHandler(error){
+
+  console.log("error occurred",error);
+  alert("something wrong with server,try later")
+}
+
+
+
 function clickHandler(){
-  console.log("clicked!");
-console.log("input",txtInput.value);
+ 
+ var inputText = txtInput.value; //taking input
+
+ //calling server for processing
+fetch (getTranslationURL(inputText))
+.then(response=>response.json())
+.then(json=>{
+  var translatedText=json.contents.translated;
+  outputDiv.innerText = translatedText;
+})
+
+.catch(errorHandler)
 
 
 };
